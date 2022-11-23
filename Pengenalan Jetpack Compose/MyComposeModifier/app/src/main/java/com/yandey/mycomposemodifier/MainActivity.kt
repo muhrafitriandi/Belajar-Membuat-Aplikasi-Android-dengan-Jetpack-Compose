fd29c109d0f31e8143a6bfa0c251da766a6f6aff
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-11-23 23:31:19 +0700

    create card, preview parameter

--- Pengenalan Jetpack Compose/MyComposeModifier/app/src/main/java/com/yandey/mycomposemodifier/MainActivity.kt
+++ Pengenalan Jetpack Compose/MyComposeModifier/app/src/main/java/com/yandey/mycomposemodifier/MainActivity.kt
@@ -3,13 +3,28 @@ package com.yandey.mycomposemodifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import com.yandey.mycomposemodifier.ui.theme.MyComposeModifierTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +35,25 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
                     Greeting("Android")
                 }
             }
         }
     }
+}
+
+data class User(
+    val name: String
+)
+
+class UserPreviewParameterProvider : PreviewParameterProvider<User> {
+    override val values: Sequence<User> = sequenceOf(
+        User("Muhammad Rafi Triandi"),
+        User("Rangga Madhipa Sutra Jiwa Cordosega Akre Askhala Mughal Iikhanat Akbar")
+    )
+
 }
 
 @Composable
@@ -36,8 +63,56 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreview(@PreviewParameter(UserPreviewParameterProvider::class) user: User) {
     MyComposeModifierTheme {
-        Greeting("Android")
+        ContactCard(user = user)
+    }
+}
+
+@Composable
+fun ContactCard(user: User) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Card(
+            modifier = Modifier
+                .wrapContentSize()
+                .clip(RoundedCornerShape(4.dp))
+                .background(color = Color.White)
+                .padding(16.dp)
+        ) {
+            Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max)) {
+                ContactPicture()
+                ContactContent(user.name)
+            }
+        }
+    }
+}
+
+@Composable
+fun ContactPicture() {
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(2.dp, color = Color.Green),
+        modifier = Modifier
+            .size(48.dp)
+            .padding(4.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.image_person),
+            contentDescription = "Profile Picture",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(48.dp)
+        )
+    }
+}
+
+@Composable
+fun ContactContent(name: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(6.dp), verticalArrangement = Arrangement.aligned(Alignment.CenterVertically)
+    ) {
+        Text(text = name, fontWeight = FontWeight.Bold)
+        Text(text = "Online")
     }
 }
