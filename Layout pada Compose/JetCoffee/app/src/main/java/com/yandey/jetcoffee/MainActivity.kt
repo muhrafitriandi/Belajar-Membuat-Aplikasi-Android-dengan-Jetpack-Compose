5c19085ec1a80a2565ddeac43790eba7c22cf486
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-11-26 11:28:57 +0700

    Create Bottom Nav

--- Layout pada Compose/JetCoffee/app/src/main/java/com/yandey/jetcoffee/MainActivity.kt
+++ Layout pada Compose/JetCoffee/app/src/main/java/com/yandey/jetcoffee/MainActivity.kt
@@ -9,20 +9,21 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.yandey.jetcoffee.model.Menu
-import com.yandey.jetcoffee.model.dummyBestSellerMenu
-import com.yandey.jetcoffee.model.dummyCategory
-import com.yandey.jetcoffee.model.dummyMenu
+import com.yandey.jetcoffee.model.*
 import com.yandey.jetcoffee.ui.components.*
 import com.yandey.jetcoffee.ui.theme.JetCoffeeTheme
 
@@ -45,26 +46,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Banner()
-        HomeSection(
-            title = stringResource(R.string.section_category),
-            content = { CategoryRow() }
-        )
-        HomeSection(
-            title = stringResource(R.string.menu_favorite),
-            content = { MenuRow(dummyMenu)}
-        )
-        HomeSection(
-            title = stringResource(R.string.section_best_seller_menu),
-            content = { MenuRow(dummyBestSellerMenu)}
-        )
+    Scaffold(bottomBar = {
+        BottomBar()
+    }) {
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            Banner()
+            HomeSection(
+                title = stringResource(R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(
+                title = stringResource(R.string.menu_favorite),
+                content = { MenuRow(dummyMenu) }
+            )
+            HomeSection(
+                title = stringResource(R.string.section_best_seller_menu),
+                content = { MenuRow(dummyBestSellerMenu) }
+            )
+        }
     }
 }
-
-
-
-
 
 @Composable
 fun Banner(
@@ -107,6 +108,46 @@ fun MenuRow(
     ) {
         items(listMenu, key = { it.title }) { menu ->
             MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            ),
+        )
+        navigationItems.map {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                },
+                label = {
+                    Text(it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                unselectedContentColor = LightGray,
+                onClick = {}
+            )
         }
     }
 }
