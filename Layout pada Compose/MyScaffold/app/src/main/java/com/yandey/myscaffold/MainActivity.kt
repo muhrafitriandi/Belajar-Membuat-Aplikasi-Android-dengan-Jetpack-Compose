34846165a8f27ce51fc90ce053a033ee7a23c06e
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-11-26 10:04:45 +0700

    fab, topappbar

--- Layout pada Compose/MyScaffold/app/src/main/java/com/yandey/myscaffold/MainActivity.kt
+++ Layout pada Compose/MyScaffold/app/src/main/java/com/yandey/myscaffold/MainActivity.kt
@@ -3,13 +3,18 @@ package com.yandey.myscaffold
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yandey.myscaffold.ui.theme.MyScaffoldTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +27,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ScaffoldPreview() {
     MyScaffoldTheme {
-        Greeting("Android")
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "My Scaffold")
+                    },
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+                }
+            },
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Hello World")
+            }
+        }
     }
 }
