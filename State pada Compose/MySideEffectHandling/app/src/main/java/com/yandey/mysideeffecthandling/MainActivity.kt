d813344e28501a090d7bd8d4cff315131252d2a5
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-11-28 02:26:26 +0700

    Composable Lifecycle

--- State pada Compose/MySideEffectHandling/app/src/main/java/com/yandey/mysideeffecthandling/MainActivity.kt
+++ State pada Compose/MySideEffectHandling/app/src/main/java/com/yandey/mysideeffecthandling/MainActivity.kt
@@ -3,13 +3,18 @@ package com.yandey.mysideeffecthandling
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yandey.mysideeffecthandling.ui.theme.MySideEffectHandlingTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DefaultPreview()
                 }
             }
         }
@@ -30,14 +35,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Counter() {
+    var count by remember { mutableStateOf(0) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        if(count > 0) {
+            Text("Button clicked $count times:")
+        }
+        Button(onClick = { count++ }) {
+            Text("Click me!")
+        }
+        Button(onClick = { count = 0 }) {
+            Text("Reset")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MySideEffectHandlingTheme {
-        Greeting("Android")
+        Counter()
     }
 }
