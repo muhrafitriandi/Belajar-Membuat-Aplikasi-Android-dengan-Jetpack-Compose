f3dda0faeb274ffebaa8937501c60ef6fed74158
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-11-28 00:21:18 +0700

    Stateful Composable

--- State pada Compose/MyStateHoisting/app/src/main/java/com/yandey/mystatehoisting/MainActivity.kt
+++ State pada Compose/MyStateHoisting/app/src/main/java/com/yandey/mystatehoisting/MainActivity.kt
@@ -3,14 +3,23 @@ package com.yandey.mystatehoisting
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yandey.mystatehoisting.ui.theme.MyStateHoistingTheme
+import androidx.compose.runtime.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +31,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,14 +38,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(16.dp)
+    ) {
+        var count by rememberSaveable {
+            mutableStateOf(0)
+        }
+        Text("Button clicked $count times:")
+        Button(onClick = { count++ }) {
+            Text("Click me!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun CounterPreview() {
     MyStateHoistingTheme {
-        Greeting("Android")
+        StatefulCounter()
     }
 }
