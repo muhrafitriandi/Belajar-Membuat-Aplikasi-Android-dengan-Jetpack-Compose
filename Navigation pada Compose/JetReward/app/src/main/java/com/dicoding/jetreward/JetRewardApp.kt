723747a0f59e1c1d2d0a4fddba2c71250e96c16a
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-04 14:48:45 +0700

    Showing Route Bottom Bar

--- Navigation pada Compose/JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ Navigation pada Compose/JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -1,9 +1,7 @@
 package com.dicoding.jetreward
 
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
@@ -12,19 +10,49 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.navigation.Screen
+import com.dicoding.jetreward.ui.screen.cart.CartScreen
+import com.dicoding.jetreward.ui.screen.home.HomeScreen
+import com.dicoding.jetreward.ui.screen.profile.ProfileScreen
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
 
 @Composable
 fun JetRewardApp(
     modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
 ) {
-
+    Scaffold(
+        bottomBar = {
+            BottomBar(navController)
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen()
+            }
+            composable(Screen.Cart.route) {
+                CartScreen()
+            }
+            composable(Screen.Profile.route) {
+                ProfileScreen()
+            }
+        }
+    }
 }
 
 @Composable
 private fun BottomBar(
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     BottomNavigation(
