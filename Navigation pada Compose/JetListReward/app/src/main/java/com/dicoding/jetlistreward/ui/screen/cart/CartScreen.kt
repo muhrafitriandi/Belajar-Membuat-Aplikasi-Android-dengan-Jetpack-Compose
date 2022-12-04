72332fe8fae9507a254842dd850252f43bb1c83c
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-04 15:34:52 +0700

    onCart

--- Navigation pada Compose/JetListReward/app/src/main/java/com/dicoding/jetlistreward/ui/screen/cart/CartScreen.kt
+++ Navigation pada Compose/JetListReward/app/src/main/java/com/dicoding/jetlistreward/ui/screen/cart/CartScreen.kt
@@ -1,22 +1,93 @@
 package com.dicoding.jetlistreward.ui.screen.cart
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.dicoding.jetlistreward.R
+import com.dicoding.jetlistreward.di.Injection
+import com.dicoding.jetlistreward.ui.ViewModelFactory
+import com.dicoding.jetlistreward.ui.common.UiState
+import com.dicoding.jetlistreward.ui.components.CartItem
+import com.dicoding.jetlistreward.ui.components.OrderButton
 
 @Composable
 fun CartScreen(
+    viewModel: CartViewModel = viewModel(
+        factory = ViewModelFactory(
+            Injection.provideRepository()
+        )
+    )
+) {
+    viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+        when (uiState) {
+            is UiState.Loading -> {
+                viewModel.getAddedOrderRewards()
+            }
+            is UiState.Success -> {
+                CartContent(
+                    uiState.data,
+                    onProductCountChanged = { rewardId, count ->
+                        viewModel.updateOrderReward(rewardId, count)
+                    },
+                )
+            }
+            is UiState.Error -> {}
+        }
+    }
+}
+
+@Composable
+fun CartContent(
+    state: CartState,
+    onProductCountChanged: (id: Long, count: Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = modifier.fillMaxSize()
     ) {
-        Text(stringResource(R.string.menu_cart))
+        TopAppBar(backgroundColor = MaterialTheme.colors.surface) {
+            Text(
+                text = stringResource(R.string.menu_cart),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+        OrderButton(
+            text = stringResource(R.string.total_order, state.totalRequiredPoint),
+            enabled = state.orderReward.isNotEmpty(),
+            onClick = {},
+            modifier = Modifier.padding(16.dp)
+        )
+        LazyColumn(
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(state.orderReward, key = { it.reward.id }) { item ->
+                CartItem(
+                    rewardId = item.reward.id,
+                    image = item.reward.image,
+                    title = item.reward.title,
+                    totalPoint = item.reward.requiredPoint * item.count,
+                    count = item.count,
+                    onProductCountChanged = onProductCountChanged,
+                )
+                Divider()
+            }
+        }
     }
 }
