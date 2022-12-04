3f6e5cad2c8ca50ec94b701a5c396d9c5a28d223
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-04 14:53:51 +0700

    Bottom Bar Selected

--- Navigation pada Compose/JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ Navigation pada Compose/JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,6 +15,7 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.navigation.Screen
@@ -59,6 +61,9 @@ private fun BottomBar(
     BottomNavigation(
         modifier = modifier
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(R.string.menu_home),
@@ -86,7 +91,7 @@ private fun BottomBar(
                         )
                     },
                     label = { Text(item.title) },
-                    selected = true,
+                    selected = currentRoute == item.screen.route,
                     onClick = {
                         navController.navigate(item.screen.route) {
                             popUpTo(navController.graph.findStartDestination().id) {
