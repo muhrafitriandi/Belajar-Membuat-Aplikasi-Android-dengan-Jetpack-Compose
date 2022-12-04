beea4ea9fb819ce7817e7ef5a2502bfce0dc76ab
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-04 14:52:23 +0700

    Bottom Bar onClick

--- Navigation pada Compose/JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ Navigation pada Compose/JetReward/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -86,7 +87,15 @@ private fun BottomBar(
                     },
                     label = { Text(item.title) },
                     selected = true,
-                    onClick = {}
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
                 )
             }
         }
