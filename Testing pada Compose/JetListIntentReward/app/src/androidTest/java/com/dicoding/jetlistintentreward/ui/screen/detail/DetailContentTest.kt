996dc1bd5083a73e7ed37ecd5a0870a5135f1074
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-06 17:30:41 +0700

    Use FakeData to Show Detail Content

--- Testing pada Compose/JetListIntentReward/app/src/androidTest/java/com/dicoding/jetlistintentreward/ui/screen/detail/DetailContentTest.kt
+++ Testing pada Compose/JetListIntentReward/app/src/androidTest/java/com/dicoding/jetlistintentreward/ui/screen/detail/DetailContentTest.kt
@@ -2,10 +2,14 @@ package com.dicoding.jetlistintentreward.ui.screen.detail
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.dicoding.jetlistintentreward.model.OrderReward
 import com.dicoding.jetlistintentreward.model.Reward
 import org.junit.Rule
 import com.dicoding.jetlistintentreward.R
+import com.dicoding.jetlistintentreward.ui.theme.JetRewardTheme
+import org.junit.Before
 
 class DetailContentTest {
     @get:Rule
@@ -15,4 +19,21 @@ class DetailContentTest {
         reward = Reward(4, R.drawable.reward_4, "Jaket Hoodie Dicoding", 7500),
         count = 0
     )
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            JetRewardTheme {
+                DetailContent(
+                    fakeOrderReward.reward.image,
+                    fakeOrderReward.reward.title,
+                    fakeOrderReward.reward.requiredPoint,
+                    fakeOrderReward.count,
+                    onBackClick = {},
+                    onAddToCart = {}
+                )
+            }
+        }
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+    }
 }
