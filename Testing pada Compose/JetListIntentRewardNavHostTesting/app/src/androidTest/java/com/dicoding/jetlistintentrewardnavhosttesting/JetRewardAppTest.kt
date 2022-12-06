1ad81a965ed2707e79b65c5b6315a83ed0d29be8
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-06 18:19:43 +0700

    Create Compose Rule and TestNavHostController

---
+++ Testing pada Compose/JetListIntentRewardNavHostTesting/app/src/androidTest/java/com/dicoding/jetlistintentrewardnavhosttesting/JetRewardAppTest.kt
@@ -0,0 +1,27 @@
+package com.dicoding.jetlistintentrewardnavhosttesting
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.dicoding.jetlistintentrewardnavhosttesting.ui.theme.JetRewardTheme
+import org.junit.Before
+import org.junit.Rule
+
+class JetRewardAppTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            JetRewardTheme {
+                navController = TestNavHostController(LocalContext.current)
+                navController.navigatorProvider.addNavigator(ComposeNavigator())
+                JetRewardApp(navController = navController)
+            }
+        }
+    }
+}
