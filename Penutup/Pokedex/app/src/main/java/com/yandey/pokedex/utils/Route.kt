f8b61c2f830d6c6fa6cfca759a7552632038205f
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-08 13:04:00 +0700

    Create Route Constanta

---
+++ Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/utils/Route.kt
@@ -0,0 +1,8 @@
+package com.yandey.pokedex.utils
+
+object Route {
+    const val HOME_ROUTE = "home"
+    const val FAVORITE_ROUTE = "favorite"
+    const val PROFILE_ROUTE = "profile"
+    const val DETAIL_ROUTE = "home/{id}"
+}
