10002d10d7e5473332c3863bbd9b2984ad204ac3
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-08 13:00:35 +0700

    Create Sealed Class Screen with Route

---
+++ Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/ui/navigation/Screen.kt
@@ -0,0 +1,15 @@
+package com.yandey.pokedex.ui.navigation
+
+import com.yandey.pokedex.utils.Route.DETAIL_ROUTE
+import com.yandey.pokedex.utils.Route.FAVORITE_ROUTE
+import com.yandey.pokedex.utils.Route.HOME_ROUTE
+import com.yandey.pokedex.utils.Route.PROFILE_ROUTE
+
+sealed class Screen(val route: String) {
+    object Home : Screen(HOME_ROUTE)
+    object Favorite : Screen(FAVORITE_ROUTE)
+    object Profile : Screen(PROFILE_ROUTE)
+    object DetailMonster : Screen(DETAIL_ROUTE) {
+        fun createRoute(id: Long) = "$HOME_ROUTE/$id"
+    }
+}
