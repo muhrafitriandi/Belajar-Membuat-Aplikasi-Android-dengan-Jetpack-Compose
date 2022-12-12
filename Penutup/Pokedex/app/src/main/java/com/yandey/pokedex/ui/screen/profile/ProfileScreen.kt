4a25fc44d2889d90bf02319dadf13a836d3804b6
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-12 11:56:41 +0700

    Create Image, Name, Email Profile Screen

--- Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/ui/screen/profile/ProfileScreen.kt
+++ Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/ui/screen/profile/ProfileScreen.kt
@@ -1,13 +1,20 @@
 package com.yandey.pokedex.ui.screen.profile
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.yandey.pokedex.R
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.yandey.pokedex.ui.components.Title
 
 @Composable
 fun ProfileScreen(
@@ -17,8 +24,37 @@ fun ProfileScreen(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Text(
-            text = stringResource(id = R.string.menu_profile)
-        )
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = modifier.height(16.dp))
+            AsyncImage(
+                modifier = modifier
+                    .size(250.dp, 250.dp)
+                    .clip(RoundedCornerShape(250.dp))
+                    .background(color = MaterialTheme.colors.onSurface),
+                model = "https://media-exp1.licdn.com/dms/image/D5603AQFKtLaOs8doPw/profile-displayphoto-shrink_800_800/0/1669266249560?e=1676505600&v=beta&t=vs5Gkg8elP073JHpLsF8q7P8yLWwnPMUAZ5bpM5E2VI",
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = modifier.height(24.dp))
+            Title(
+                title = "Muhammad Rafi Triandi",
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colors.surface
+            )
+            Spacer(modifier = modifier.height(24.dp))
+            Title(
+                title = "muhammadrafitriandi@gmail.com",
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.surface
+            )
+            Spacer(modifier = modifier.height(16.dp))
+        }
     }
 }
