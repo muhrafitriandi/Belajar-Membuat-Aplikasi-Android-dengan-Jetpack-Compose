24e6922ac39e2ba37cbcb1c5903c1c07592262d4
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-12 10:42:21 +0700

    Remove Unusued Resource

--- Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/ui/components/MonsterInfo.kt
+++ Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/ui/components/MonsterInfo.kt
@@ -6,15 +6,11 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.yandey.pokedex.R
 import com.yandey.pokedex.data.FakeMonsterDataSource
 import com.yandey.pokedex.data.models.Monster
