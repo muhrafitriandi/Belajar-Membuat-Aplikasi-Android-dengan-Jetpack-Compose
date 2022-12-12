34a15008c7db9324b5df10649bfa5c22319395d1
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2022-12-12 10:34:42 +0700

    Show Empty Search Data

--- Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/ui/screen/home/HomeScreen.kt
+++ Penutup/Pokedex/app/src/main/java/com/yandey/pokedex/ui/screen/home/HomeScreen.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.yandey.pokedex.data.models.Monster
 import com.yandey.pokedex.ui.common.UiState
+import com.yandey.pokedex.ui.components.EmptyData
 import com.yandey.pokedex.ui.components.MonsterItem
 import com.yandey.pokedex.ui.components.SearchBar
+import com.yandey.pokedex.R
 
 @Composable
 fun HomeScreen(
@@ -49,13 +52,30 @@ fun HomeContent(
     query: String,
     onQueryChange: (String) -> Unit
 ) {
+    Column {
+        SearchBar(query = query, onQueryChange = onQueryChange)
+        if (monsters.isNotEmpty()) {
+            ShowMonsters(
+                modifier = modifier,
+                monsters = monsters,
+                onItemClick = onItemClick
+            )
+        } else {
+            EmptyData(text = stringResource(id = R.string.pokemon_not_found))
+        }
+    }
+}
+
+@Composable
+fun ShowMonsters(
+    modifier: Modifier = Modifier,
+    monsters: List<Monster>,
+    onItemClick: (id: Long) -> Unit
+) {
     LazyColumn(
         contentPadding = PaddingValues(8.dp),
         modifier = modifier
     ) {
-        item {
-            SearchBar(query = query, onQueryChange = onQueryChange)
-        }
         items(monsters) { monster ->
             MonsterItem(monster = monster) { id ->
                 onItemClick(id)
